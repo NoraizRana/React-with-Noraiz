6e1f08bc0d00d5005496cc5abecbe529857bf639
Noraiz Rana <noraizrana389@gmail.com>
2025-06-30 11:59:00 +0500

    Customized React using App.jsx in vite react added

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import React from 'react'
+
+const user = "Think and Code";
+const reactElement = React.createElement(
+  'a',
+  { href: 'https://google.com', target: "_blank" },
+  'Click me to visit Google'
+)
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  reactElement
 )
