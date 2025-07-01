bae2cdf51eeb1bae83238d20cbead88b645e9861
Noraiz Rana <noraizrana389@gmail.com>
2025-07-01 19:16:53 +0500

    Tailwind CSS and Props in React Added

---
+++ 03Tailwind-props/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1 className='bg-orange-400 rounded-xl text-blue-700 mb-4 text-xl'>Tailwind CSS Loaded</h1>
+     <Card username='Mehar' btntext='Show more'/>
+     <br />
+     <Card btntext='Want more?'/>
+    </>
+  )
+}
+
+export default App
