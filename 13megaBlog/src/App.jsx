29d5c3590984039892b81160b845f5dd044ffb8e
Noraiz Rana <noraizrana389@gmail.com>
2025-08-01 11:50:00 +0500

    Mega Blog Modified

--- 13megaBlog/src/App.jsx
+++ 13megaBlog/src/App.jsx
@@ -1,15 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {useDispatch} from 'react-redux'
+import authService from "./appwrite/auth"
+import {login, logout} from "./store/authSlice"
 import './App.css'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
-  
+ const [loading, setLoading]=useState(true);
+ const dispatch =useDispatch()
+ useEffect(()=>{
+  authService.getCurrentUser().
+  then((userData)=>{
+    if(userData)
+    {
+      dispatch(login({userData}))
+    }
+    else{
+      dispatch(logout())
+    }
+  })
+  .finally(()=>setLoading(false))
 
-  return (
-    <>
-     <h1 className='p-4 m-2'>AoA from Aiz. I am here waiting for my death or life in my life's hands </h1>
-    </>
-  )
+ },[])
+
+  return  !loading () : null;
 }
 
 export default App
