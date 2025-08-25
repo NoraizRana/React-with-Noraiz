7ebe08958609443fd6a256415cc1f1bca612eac9
Noraiz Rana <noraizrana389@gmail.com>
2025-08-25 19:14:20 +0500

    Mega Blog modified

--- 13megaBlog/src/components/Logo.jsx
+++ 13megaBlog/src/components/Logo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Logo({width : '100px'}) {
     return (
-       <div>This is a smaple logo</div>
+       <div>This is a smaple logo for the project Mega Blog</div>
     )
 }
 
